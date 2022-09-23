e826d14ffe6f834c56a9551bdf1569ddf441d595
CBrunote <christian.bruno1995@gmail.com>
2022-09-23 19:15:43 -0400

    Added DB connection, imported npm packages, created inital inquirer questions and queries that execute showing all in each table

--- db/schema.sql
+++ db/schema.sql
@@ -3,25 +3,25 @@ CREATE DATABASE company_db;
 
 USE company_db;
 
-CREATE TABLE department (
+CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
+    name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
     ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY manager_id REFERENCES employee(id)
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
--- index.js
+++ index.js
@@ -2,3 +2,48 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
 
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'root',
+      database: 'company_db'
+    },
+    console.log(`Connected to the company_db database.`)
+  );
+
+inquirer.prompt(
+  [
+    {
+      type: 'list',
+      message: 'What would you like to do?',
+      name: 'action',
+      choices: [
+        'View All Departments',
+        'View All Roles',
+        'View All Employees',
+        'Add Department',
+        'Add Role',
+        'Add Employee',
+        'Update Employee role',
+        'Quit'
+      ]
+    }
+  ])
+  .then((answers) => {
+    if (answers.action === 'View All Departments') {
+          db.query('SELECT * FROM departments', function (err, results) {
+            console.table(results);
+    })
+    }
+    if (answers.action === 'View All Roles') {
+      db.query('SELECT * FROM roles', function (err, results) {
+        console.table(results);
+    })
+    }
+    if (answers.action === 'View All Employees') {
+      db.query('SELECT * FROM employees', function (err, results) {
+        console.table(results);
+    })
+    }
+  });
