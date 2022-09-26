0c43df7c1b9e5d1827035cac530b18817f5accb5
CBruno <christian.bruno1995@gmail.com>
2022-09-26 18:10:04 -0400

    pushed to switch computers

--- index.js
+++ index.js
@@ -12,38 +12,79 @@ const db = mysql.createConnection(
     console.log(`Connected to the company_db database.`)
   );
 
-inquirer.prompt(
-  [
-    {
-      type: 'list',
-      message: 'What would you like to do?',
-      name: 'action',
-      choices: [
-        'View All Departments',
-        'View All Roles',
-        'View All Employees',
-        'Add Department',
-        'Add Role',
-        'Add Employee',
-        'Update Employee role',
-        'Quit'
-      ]
-    }
-  ])
-  .then((answers) => {
-    if (answers.action === 'View All Departments') {
-          db.query('SELECT * FROM departments', function (err, results) {
-            console.table(results);
-    })
-    }
-    if (answers.action === 'View All Roles') {
-      db.query('SELECT * FROM roles', function (err, results) {
-        console.table(results);
-    })
-    }
-    if (answers.action === 'View All Employees') {
-      db.query('SELECT * FROM employees', function (err, results) {
-        console.table(results);
-    })
-    }
-  });
+db.connect(function (err) {
+  if (err) throw err;
+  console.log(`connected as id ${db.threadId}`);
+  init();
+});
+
+
+function init() {
+
+    inquirer.prompt(
+      [
+        {
+          type: 'list',
+          message: 'What would you like to do?',
+          name: 'startQuestions',
+          choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role', 'Exit']
+        }
+      ])
+      .then((answers) => {
+
+        switch (answers.startQuestions) {
+          case 'View All Departments':
+            showAllDepartments();
+            break;
+          
+          case "View All Roles":
+            showAllRoles();
+            break;
+
+          case "View All Employees":
+            showAllEmployees();
+            break;
+
+          case "Add Department":
+            addDepartment();
+            break;
+
+          case "Add Role":
+            addRole();
+            break;
+
+          case "Add Employee":
+            addEmployee();
+            break;
+
+          case "Update Employee Role":
+            updateEmmployeeRole();
+            break;
+
+          case "Exit":
+            db.end();
+            break;
+        }
+      })
+};
+
+function showAllDepartments() {
+  db.query('SELECT * FROM departments', function (err, results) {
+    console.table(results);
+  })
+  .then(init());
+};
+
+function showAllRoles() {
+  db.query('SELECT * FROM roles', function (err, results) {
+    console.table(results);
+  })
+  .then(init());
+};
+
+function showAllEmployees() {
+  db.query('SELECT * FROM employees', function (err, results) {
+    console.table(results);
+  })
+  .then(init());
+};
